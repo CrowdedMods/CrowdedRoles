aa349e5d63d2cef2ae2a74f6e73971bfe69fb7c9
Galster-dev <segom0574@gmail.com>
2021-02-06 18:07:16 +0300

    enabled kill button

--- CrowdedRoles.Api/Patches/KillPatches.cs
+++ CrowdedRoles.Api/Patches/KillPatches.cs
@@ -1,4 +1,5 @@
 ﻿using CrowdedRoles.Api.Extensions;
+using CrowdedRoles.Api.Roles;
 using HarmonyLib;
 
 namespace CrowdedRoles.Api.Patches
@@ -26,5 +27,49 @@ namespace CrowdedRoles.Api.Patches
                 return false;
             }
         }
+
+        // Patches all (i hope) methods disabling kill button
+        [HarmonyPatch]
+        private static class KillButtonSetActivePatches
+        {
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(HudManager), nameof(HudManager.SetHudActive))]
+            private static void HudManager_SetHudActive(HudManager __instance)
+            {
+                BaseRole? role = PlayerControl.LocalPlayer.GetRole();
+                if (role != null)
+                {
+                    __instance.KillButton.gameObject.SetActive(role.AbleToKill);
+                }
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Revive))]
+            private static void PlayerControl_Revive(PlayerControl __instance)
+            {
+                if (__instance.AmOwner)
+                {
+                    BaseRole? role = __instance.GetRole();
+                    if (role != null)
+                    {
+                        HudManager.Instance.KillButton.gameObject.SetActive(role.AbleToKill);
+                    }
+                }
+            }
+
+            [HarmonyPostfix]
+            [HarmonyPatch(typeof(PlayerControl.CoSetTasks__d), nameof(PlayerControl.CoSetTasks__d.MoveNext))]
+            private static void PlayerControl_CoSetTasks(PlayerControl.CoSetTasks__d __instance, bool __result)
+            {
+                if (__result && __instance.__this.AmOwner) // yield break
+                {
+                    BaseRole? role = __instance.__this.GetRole();
+                    if (role != null)
+                    {
+                        HudManager.Instance.KillButton.gameObject.SetActive(role.AbleToKill);
+                    }
+                }
+            }
+        }      
     }
 }
