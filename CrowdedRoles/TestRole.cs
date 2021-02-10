8dfbf46b4db4620a854b346ac18dd77c572db0ee
Galster-dev <segom0574@gmail.com>
2021-02-10 17:25:22 +0300

    updated test role

--- CrowdedRoles/TestRole.cs
+++ CrowdedRoles/TestRole.cs
@@ -1,6 +1,7 @@
 ﻿#if DEBUG
 using BepInEx.IL2CPP;
 using CrowdedRoles.Attributes;
+using CrowdedRoles.Extensions;
 using CrowdedRoles.Roles;
 using UnityEngine;
 
@@ -16,12 +17,13 @@ namespace CrowdedRoles
         public override string Name { get; } = "TestRole";
         public override Color Color { get; } = Color.cyan;
         public override Visibility Visibility { get; } = Visibility.Team;
-        public override string StartTip { get; } = "say meow pls";
+        public override string Description { get; } = "say meow pls";
         public override bool AbleToKill { get; } = true;
         public override Side Side { get; } = Side.Impostor;
-        public override bool PreKill(ref PlayerControl killer, ref PlayerControl target, ref bool noSnap)
+        public override bool PreKill(ref PlayerControl killer, ref PlayerControl target, ref CustomMurderOptions options)
         {
-            return noSnap = true;
+            options |= CustomMurderOptions.NoAnimation | CustomMurderOptions.NoSnap;
+            return true;
         }
     }
 }
